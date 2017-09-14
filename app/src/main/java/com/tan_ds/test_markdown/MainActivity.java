437ed2f805eafa9b272f564c291fb340f0c051bf
thanator <www.godx@mail.ru>
2017-09-14 15:33:07 +0300

    update 3 - refactoring code

--- app/src/main/java/com/tan_ds/test_markdown/MainActivity.java
+++ app/src/main/java/com/tan_ds/test_markdown/MainActivity.java
@@ -1,31 +1,20 @@
 package com.tan_ds.test_markdown;
 
-import android.content.Context;
-import android.content.res.Resources;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import org.markdown4j.Markdown4jProcessor;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText myEditText;
-    private String myTextHTML, MyUrl;
+    private String myUrl;
     private WebView myWebView;
 
     @Override
@@ -36,53 +25,36 @@ public class MainActivity extends AppCompatActivity {
         myEditText = (EditText) findViewById(R.id.myEditText);
     }
 
-
     public void Shout(View view) {
-
-        Context tContext = getApplicationContext();
-
         if (!myEditText.getText().toString().isEmpty()){
-            MyUrl = myEditText.getText().toString();
-        } else{
-            MyUrl = "";
-            Toast toast = Toast.makeText(getApplicationContext(), "This shit is empty", Toast.LENGTH_SHORT);
-            toast.show();
-        }
-
-
-
-        getSupportLoaderManager().restartLoader(999, null, new downloadCallBack());
-
-
+                myUrl = myEditText.getText().toString();
+            } else{
+                myUrl = "";
+                Toast toast = Toast.makeText(getApplicationContext(), "This shit is empty", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+            getSupportLoaderManager().restartLoader(999, null, new downloadCallBack());
         }
 
         private class downloadCallBack implements LoaderManager.LoaderCallbacks <String>{
 
             @Override
             public Loader<String> onCreateLoader(int id, Bundle args) {
-                return new docReader(MainActivity.this, MyUrl);  // https://vk.com/doc48817927_450398490
+                return new DocReader(MainActivity.this, myUrl); 
             }
 
             @Override
             public void onLoadFinished(Loader<String> loader, String data) {
-
                 try {
-                    String test_Markdown = new Markdown4jProcessor().process(data);
-                    myWebView.loadData(test_Markdown, "text/html; charset=utf-8", "utf-8");
+                    String testMarkdown = new Markdown4jProcessor().process(data);
+                    myWebView.loadData(testMarkdown, "text/html; charset=utf-8", "utf-8");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
-
-
-
             }
 
             @Override
             public void onLoaderReset(Loader<String> loader) {
-
             }
         }
-
-
 }
